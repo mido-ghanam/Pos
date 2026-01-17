bb036d740e86cde23880be39347f1bcfeb8403e3
MidoGhanam-Lab <midoghanam@hotmail.com>
2026-01-17 14:51:12 +0200

    | 17-01-2026 | 14:51:11 |

--- products/serializers.py
+++ products/serializers.py
@@ -8,16 +8,17 @@ class AddCategoriesSerializer(serializers.Serializer): name = serializers.CharFi
   
 class AllProductsSerializer(serializers.ModelSerializer):
   category = serializers.ReadOnlyField(source="category.name")
-  class Meta: model, fields = Products, ('id', 'name', 'category', "barcode", "quantity", 'buy_price', "sell_price", "min_quantity", "active", "monitor")
+  class Meta: model, fields = Products, ('id', 'name', 'category', "large_count", "barcode", "quantity", 'buy_price', "sell_price", "min_quantity", "active", "monitor")
 
 class GetProductSerializer(serializers.ModelSerializer):
   category = CategoriesSerializer()
-  class Meta: model, fields = Products, ('id', 'name', 'barcode', 'category', 'discription', 'buy_price', "sell_price", "quantity", "min_quantity", "active", "monitor")
+  class Meta: model, fields = Products, ('id', 'name', 'barcode', "large_count", 'category', 'discription', 'buy_price', "sell_price", "quantity", "min_quantity", "active", "monitor")
 
 class AddProductSerializer(serializers.Serializer):
   name = serializers.CharField()
   barcode = serializers.IntegerField()
   category = serializers.CharField()
+  large_count = serializers.IntegerField(required=False)
   discription = serializers.CharField(required=False, allow_blank=True)
   buy_price = serializers.FloatField()
   sell_price = serializers.FloatField()
@@ -27,5 +28,5 @@ class AddProductSerializer(serializers.Serializer):
   monitor = serializers.BooleanField(required=False, default=False)
 
 class EditProductSerializer(serializers.ModelSerializer): 
-  class Meta: model, fields = Products, ["name", "price", "category", "description"]
+  class Meta: model, fields = Products, ["name", "price", "category", "description", "large_count"]
 
